8fb988a201470a16766917a128379a36fa266c01
sdldev <sidolapakdev@gmail.com>
2025-01-01 20:01:33 +0700

    Update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,15 +1,34 @@
-// API endpoints
+interface Post {
+  slug: string;
+  id: number;  
+  title: string;
+  author: string;
+  category: string;
+  image: string;
+  content: string;
+  imagecaption: string;
+  created_at: string;
+
+}
+
+interface Category {
+  id: string | number;
+  title: string;
+  slug: string;
+
+}
+
 const API_BASE_URL = import.meta.env.PUBLIC_API_URL;
 
-export async function getPosts() {
+export async function getPosts(): Promise<Post[]> {
   const response = await fetch(`${API_BASE_URL}/api/posts`);
   const data = await response.json();
   return data.data;
 }
 
-export async function getPostBySlug(slug: string) {
-  const posts = await getPosts();
-  const post = posts.find(post => post.slug === slug);
+export async function getPostBySlug(slug: string): Promise<Post | null> {
+  const posts: Post[] = await getPosts();
+  const post: Post | undefined = posts.find((post: Post) => post.slug === slug);
   
   if (!post) return null;
   
@@ -19,13 +38,13 @@ export async function getPostBySlug(slug: string) {
   return data.data;
 }
 
-export async function getCategories() {
+export async function getCategories(): Promise<Category[]> {
   const response = await fetch(`${API_BASE_URL}/api/categories`);
   const data = await response.json();
   return data.data;
 }
 
-export async function getCategory(id: string | number) {
+export async function getCategory(id: string | number): Promise<Category> {
   const response = await fetch(`${API_BASE_URL}/api/categories/${id}`);
   const data = await response.json();
   return data.data;
--- src/lib/utils/image.ts
+++ src/lib/utils/image.ts
@@ -3,7 +3,6 @@ const API_STORAGE_URL = `${import.meta.env.PUBLIC_API_URL}/storage/posts/images`
 export function getImageUrl(imageName: string): string {
   return `${API_STORAGE_URL}/${imageName}`;
 }
-
-export function getFallbackImageUrl(): string {
-  return 'https://placehold.co/600x400?text=No+Image';
-}
+export function getFallbackImageUrl(): string {  
+  return '/images/600x400.svg'; // Path relative untuk file SVG  
+}  
