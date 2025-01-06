d7fa09f6cabb43169282a68caee9c1de552161c7
sdldev <sidolapakdev@gmail.com>
2025-01-06 19:14:33 +0700

    upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'  // Enable SSR
+
+  // Enable SSR
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
